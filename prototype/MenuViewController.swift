853a1036df791c75baf72cb2a9cdddf7c7e92b70
Izzy Koningstein <izzykoningstein@Izzys-MacBook-Pro.local>
2019-11-10 13:54:25 -0500

    updates

---
+++ prototype/MenuViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MenuViewController.swift
+//  prototype
+//
+//  Created by Izzy Koningstein on 11/10/19.
+//  Copyright © 2019 Izzy Koningstein. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func backAction(_ sender: Any) {
+        self.performSegue(withIdentifier: "backToFoodMainSegue", sender: self)
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
