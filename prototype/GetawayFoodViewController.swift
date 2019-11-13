1e2888f22498bc17e15ea5d16765f6c676744485
Izzy Koningstein <izzykoningstein@0587431209.wireless.umich.net>
2019-11-12 19:05:15 -0500

    added swipe for menu items

--- prototype/GetawayFoodViewController.swift
+++ prototype/GetawayFoodViewController.swift
@@ -8,23 +8,60 @@
 
 import UIKit
 
-class GetawayFoodViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class GetawayFoodViewController: UITableViewController {
+    var mealImages = [UIImage]()
+    var mealNames = [String]()
+    
+    private func loadSampleMeals() {
+        guard let photo1 = UIImage(named: "chocolateMilkshake") else { fatalError("error") }
+        guard let photo2 = UIImage(named: "vanillaMilkshake") else { fatalError("error") }
+        guard let photo3 = UIImage(named: "strawberryMilkshake") else { fatalError("error") }
+        guard let photo4 = UIImage(named: "berrySmoothie") else { fatalError("error") }
+        guard let photo5 = UIImage(named: "greenSmoothie") else { fatalError("error") }
+        guard let photo6 = UIImage(named: "oatmeal") else { fatalError("error") }
+        guard let photo7 = UIImage(named: "acai") else { fatalError("error") }
+        guard let photo8 = UIImage(named: "granola") else { fatalError("error") }
+        guard let photo9 = UIImage(named: "muffin") else { fatalError("error") }
+        mealImages.append(photo1)
+        mealImages.append(photo2)
+        mealImages.append(photo3)
+        mealImages.append(photo4)
+        mealImages.append(photo5)
+        mealImages.append(photo6)
+        mealImages.append(photo7)
+        mealImages.append(photo8)
+        mealImages.append(photo9)
+        
+        mealNames += ["Chocolate Milkshake", "Vanilla Milkshake", "Strawberry Milkshake", "Berry Smoothie", "Green Smoothie", "Oatmeal", "Acai Bowl", "Granola", "Muffin"]
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the sample data.
+        loadSampleMeals()
     }
-    */
-
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return mealImages.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "MealTableViewCell"
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? MealTableViewCell  else {
+            fatalError("The dequeued cell is not an instance of MealTableViewCell.")
+        }
+        
+        // Fetches the appropriate meal for the data source layout.
+        let meal = mealImages[indexPath.row]
+        // Configure the cell...
+        cell.foodLabel.text = mealNames[indexPath.row]
+        cell.imageFood.image = meal
+        
+        return cell
+    }
 }
---
+++ prototype/MealTableViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  MealTableViewCell.swift
+//  prototype
+//
+//  Created by Izzy Koningstein on 11/12/19.
+//  Copyright © 2019 Izzy Koningstein. All rights reserved.
+//
+
+import UIKit
+
+
+
+class MealTableViewCell: UITableViewCell {
+     // Mark properties
+    
+  
+    @IBOutlet weak var foodLabel: UILabel!
+    @IBOutlet weak var imageFood: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    //MARK: Private Methods
+    
+ 
+    
+   
+}
