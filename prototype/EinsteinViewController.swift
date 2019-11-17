17867af2fc4c4bc2490d6292d995bae00049d5a3
Izzy Koningstein <izzykoningstein@0587380576.wireless.umich.net>
2019-11-17 12:00:23 -0500

    added swipe gesture for back segue on einstein

--- prototype/EinsteinViewController.swift
+++ prototype/EinsteinViewController.swift
@@ -11,11 +11,14 @@ import UIKit
 class EinsteinViewController: UITableViewController {
 
     
+    @IBOutlet weak var backButton: UIBarButtonItem!
     var mealImages = [UIImage]()
     var mealNames = [String]()
     var mealPrices = [String]()
 
-    
+    @IBAction func backAction(_ sender: UIBarButtonItem) {
+        self.performSegue(withIdentifier: "backSegue", sender: self)
+    }
     private func loadSampleMeals() {
         guard let photo1 = UIImage(named: "einstein_allnighter") else { fatalError("error") }
         guard let photo2 = UIImage(named: "einstein_avocadotoast") else { fatalError("error") }
@@ -47,8 +50,28 @@ class EinsteinViewController: UITableViewController {
         super.viewDidLoad()
 //        self.tableView.register(MealTableViewCell.self, forCellReuseIdentifier: "EinsteinMealTableViewCell")
         loadSampleMeals();
-
+        let rightSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe(sender:)))
+        // the default direction is right
+        view.addGestureRecognizer(rightSwipe)
         // Do any additional setup after loading the view.
+    }
+    
+    @objc func handleSwipe(sender: UISwipeGestureRecognizer) {
+        if sender.state == .ended {
+            switch sender.direction {
+            case .right:
+                view.backgroundColor = .red
+                let newViewController = UIViewController()
+                
+                self.navigationController?.pushViewController(newViewController, animated: true)
+                
+                let vc = self.storyboard?.instantiateViewController(withIdentifier: "foodView") as! UIViewController
+                
+                self.present(vc, animated: true, completion: nil)
+            default:
+                break
+            }
+        }
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
