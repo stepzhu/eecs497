dacb179261b3f25e5579e95edf575c49f1f5a925
Izzy Koningstein <izzykoningstein@0587380576.wireless.umich.net>
2019-11-17 10:12:31 -0500

    einstein food menu

--- prototype/EinsteinViewController.swift
+++ prototype/EinsteinViewController.swift
@@ -8,12 +8,69 @@
 
 import UIKit
 
-class EinsteinViewController: UIViewController {
+class EinsteinViewController: UITableViewController {
 
+    
+    var mealImages = [UIImage]()
+    var mealNames = [String]()
+    var mealPrices = [String]()
+
+    
+    private func loadSampleMeals() {
+        guard let photo1 = UIImage(named: "einstein_allnighter") else { fatalError("error") }
+        guard let photo2 = UIImage(named: "einstein_avocadotoast") else { fatalError("error") }
+        guard let photo3 = UIImage(named: "einstein_blueberrymuffin") else { fatalError("error") }
+        guard let photo4 = UIImage(named: "einstein_cinnamonwalnutstrudel") else { fatalError("error") }
+        guard let photo5 = UIImage(named: "einstein_frenchtoastbagel") else { fatalError("error") }
+        guard let photo6 = UIImage(named: "einstein_hamandswiss") else { fatalError("error") }
+        guard let photo7 = UIImage(named: "einstein_heavenlychocolatechip") else { fatalError("error") }
+        guard let photo8 = UIImage(named: "einstein_plainbagel") else { fatalError("error") }
+        guard let photo9 = UIImage(named: "einsteinfarmhouse") else { fatalError("error") }
+        guard let photo10 = UIImage(named: "einstein_hummusvegout") else { fatalError("error") }
+
+        mealImages.append(photo1)
+        mealImages.append(photo2)
+        mealImages.append(photo3)
+        mealImages.append(photo4)
+        mealImages.append(photo5)
+        mealImages.append(photo6)
+        mealImages.append(photo7)
+        mealImages.append(photo8)
+        mealImages.append(photo9)
+        mealImages.append(photo10)
+
+        mealNames += ["Bacon, Egg and Cheese Bagel", "Avocado Toast", "Bluberry Muffin", "Cinnamon Walnut Strudel", "French Toast Bagel", "Ham and Swiss Bagel", "Heavenly Chocolate Chip Bagel", "Plain Bagel", "Bacon, Ham, Egg and Cheese Bagel", "Hummus and Veggie Bagel"]
+        mealPrices += ["$5.39", "$4.39", "$2.50", "$3.50", "$1.20", "$5.39", "$1.20", "$1.00", "$5.39", "$5.39"]
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+//        self.tableView.register(MealTableViewCell.self, forCellReuseIdentifier: "EinsteinMealTableViewCell")
+        loadSampleMeals();
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return mealImages.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "MealTableViewCellEinstein"
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? MealTableViewCellEinstein  else {
+            fatalError("The dequeued cell is not an instance of MealTableViewCellEinstein.")
+        }
+       
+        // Fetches the appropriate meal for the data source layout.
+        // Configure the cell...
+        cell.foodLabel.text = mealNames[indexPath.row]
+        cell.foodImageReal.image = mealImages[indexPath.row]
+        
+        return cell
     }
     
 
--- prototype/MealTableViewCell.swift
+++ prototype/MealTableViewCell.swift
@@ -34,3 +34,30 @@ class MealTableViewCell: UITableViewCell {
     
    
 }
+
+
+class MealTableViewCellEinstein: UITableViewCell {
+    // Mark properties
+    
+    @IBOutlet weak var foodImageReal: UIImageView!
+    
+    @IBOutlet weak var priceLabel: UILabel!
+    
+    @IBOutlet weak var foodLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+    //MARK: Private Methods
+    
+    
+    
+    
+}
